d59ea9a7b105b5f8422930ca8b6e7d338be5f1e6
3rw1n25@gmail.com <3rw1n25@gmail.com>
2023-02-17 15:51:05 +0700

    Nambah aja

--- sang_petualang.php
+++ sang_petualang.php
@@ -1,4 +1,100 @@
 <?php
     echo "Udah sore nih... \n";
     echo "Ngopi yuk...!!!";
+
+    // echo "Hello World!" . PHP_EOL;
+    // echo "Hello World!\n";
+    // echo "Test\n";
+
+    // $durationHours = 5 ;
+    // $course = 'PHP Dasar' ;
+    // echo $durationHours.' '.$course."\n";
+    // echo "{$durationHours} {$course}";
+
+    // $umur = 40;
+    // var_dump($umur);
+    // $panjang = 40.0;
+    // var_dump($panjang);
+    // $overflow = 99898989799800;
+    // var_dump($overflow);
+
+    // $rootbeer = 'A & W';
+    // echo $rootbeer;
+
+    // echo "Ini test Simple! \n \n \n";
+
+    // $foo = true;
+    // var_dump($foo);
+    
+    //$namaMurid = array("Dhafin", "Anto", "Budi");
+    // $namaMurid = array("Bio" => "Dhafin", "Fis" => "Anto", "mt" => "Budi");
+    // var_dump($namaMurid);
+    // var_dump($namaMurid["Bio"]);
+
+    // $a = 1;
+    // $b = 2;
+    // $c = $a / $b + 1;
+    // var_dump($a + $b - 90 * 2);
+    // var_dump($c);
+
+    // $a = 1;
+    // $b = 2;
+    // $b += $a;
+    // var_dump($b);
+
+    // $x = 10;
+    // $y = '10';
+    // var_dump($x != $y); //tidak sama dengan
+    // var_dump($x == $y); //sama dengan, beda tipe di compare sama (true)
+    // var_dump($x === $y); //sama dengan, beda tipe di compare beda (false)
+
+    // $x = 10;
+    // $y = 20;
+    // var_dump($x ++);
+    // var_dump($x);
+
+    // $goodlooking = true;
+    // $kaya = false;
+    // $dapatPasangan = $goodlooking && $kaya; //false
+    // $dapatPasangan = $goodlooking || $kaya; //true
+    // var_dump($dapatPasangan);
+
+    // $namaDepan = "Ari";
+    // $namaBelakang = "Untung";
+    // var_dump($namaDepan . " ". $namaBelakang);
+
+    // $nama = "Ari";
+    // $nama .= "Untung";
+    // var_dump($nama);
+    // $arr1 = array("A" => "Apple", "B" => "Banana");
+    // $arr2 = array("A" => "Apple", "B" => "Banana", "C" => "Cherry");
+    // $arr3 = $arr1 + $arr2;
+    // var_dump($arr3);
+
+    // function writeMsg() {
+    //     $retval = "Hello World!";
+    //     return $retval;
+    // }
+
+    // echo writeMsg();
+
+    // $panjang = 10;
+    // $lebar = 20;
+    // $luasPersegiPanjang = $panjang * $lebar;
+    // $luasPersegi = $panjang * $panjang;
+    // echo "Luas Persegi Panjang adalah"." : ".$luasPersegiPanjang."\n";
+    // echo "Luas Persegi adalah"." : ".$luasPersegi;
+
+    // $x = 1;
+    // do {
+    //     echo "The number is :  $x <br>";
+    //     $x++;
+    // } while ($x <= 5);
+
+    // for ($x = 0; $x < 10; $x++) {
+    //     if ($x == 6) {
+    //         break;
+    //     }
+    //     echo "The number is : $x <br>";
+    // }
 ?>
